3199d7e9f0d123807689aef3e12b4599610cf911
swathi bawge <swathiveda4632@gmail.com>
2026-05-11 22:46:17 +0530

    Update mem_test_lib.sv

--- uvm/memory/mem_test_lib.sv
+++ uvm/memory/mem_test_lib.sv
@@ -52,6 +52,30 @@ endtask
 
 endclass
 
+class mem_five_wr_three_rd_test extends mem_base_test;
+  `uvm_component_utils(mem_five_wr_three_rd_test)
+	`NEW_COMP
+
+task run_phase(uvm_phase phase);
+	//create and instantiate the seq
+	mem_5_wr_3_rd_seq seq_h = mem_5_wr_3_rd_seq::type_id::create("seq_h");
+	//raise objection
+	phase.raise_objection(this);
+	#20;
+	//map the seq
+	seq_h.start(env_h.agt_h.sqr_h);
+
+	//drain time
+	phase.phase_done.set_drain_time(this,100);
+	#200;
+	//drop objection
+	phase.drop_objection(this);
+
+endtask
+
+
+endclass
+
 class mem_full_wr_full_rd_test extends mem_base_test;
   `uvm_component_utils(mem_full_wr_full_rd_test)
 	`NEW_COMP
